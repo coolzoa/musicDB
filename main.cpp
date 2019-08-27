8a6d6b37f03501acd5f8f013c98ddda2300ba9b5
coolzoa <jp1995azul@hotmail.com>
2019-08-27 22:33:38 +0200

    Added showMainMenu options in main

--- main.cpp
+++ main.cpp
@@ -16,25 +16,51 @@ int numberArtists = 0;
 int artistId = 0;
 int albumId = 0;
 
-Song songs[MAX];
-Album albums[MAX];
-Artist artists[MAX];
+Song* songs = NULL;
+Album* albums = NULL;
+Artist* artists = NULL;
+
+void intitializeArrays() {
+    songs = new Song[MAX];
+    albums = new Album[MAX];
+    artists = new Artist[MAX];
+}
+
+void deleteArrays() {
+    delete [] songs;
+    delete [] albums;
+    delete [] artists;
+}
+
 
 
 
 void showMainMenu() {
     cout << "=====Music Database=====" << endl;
-    cout << "1. " << endl;
-    cout << "2. " << endl;
-    cout << songs[0].toString() << endl;
+    cout << "1. Add New Artist" << endl;
+    cout << "2. Add New Album" << endl;
+    cout << "3. Add New Song" << endl;
+    cout << "4. Read a file" << endl;
+    cout << "5. Save database to a file" << endl;
+    cout << "6. Exit" << endl << endl;
+    
+    std::string option;
+    cout << "Press the option number: ";
+    getline(std::cin, option);
+    cout << "You pressed: " << option << " now we do something";
+
 }
 
 
 int main() {
+    intitializeArrays();
+    cout << songs[0].toString();
+
+
 
     
-    songs[0] = Song(1, 1, "Hello", 4.5);
-    songs[1] = Song(2, 2, "It's Me", 3.3);
+    // songs[0] = new Song(1, 1, "Hello", 4.5);
+    // songs[1] = Song(2, 2, "It's Me", 3.3);
 
 
     showMainMenu();
@@ -43,6 +69,7 @@ int main() {
     cout << Validator::isEmpty("") <<" I" << endl;
 
     cout << Validator::toLower("HEEY boii it'sa me") << endl;
+    deleteArrays();
 
     return 0;
 }
